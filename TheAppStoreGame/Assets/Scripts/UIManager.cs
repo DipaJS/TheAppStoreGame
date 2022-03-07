c6c016a693a0ecaff2ce164e117ecfc1c0ecfbb6
gusibraaz <aziz_ibrahim1@hotmail.com>
2022-03-07 16:03:00 +0100

    Accept and reject buttons
    Added TMP accept and reject buttons as well as functionality

--- TheAppStoreGame/Assets/Scripts/UIManager.cs
+++ TheAppStoreGame/Assets/Scripts/UIManager.cs
@@ -24,11 +24,34 @@ public class UIManager : MonoBehaviour
         titleField = GameObject.Find("Title").GetComponent<Text>();
         descriptionField = GameObject.Find("Description").GetComponent<Text>();
 
+
+        //GameManager.instance.Awake(); 
+
         currentApp = ((App)GameManager.instance.apps.Dequeue());
         setApp(currentApp);
     }
 
-    // Update is called once per frame
+    public void Accept()
+    {
+        Debug.Log("Accepted!");
+        GameManager.instance.AppChoice(currentApp, true); // Saves currentApp to GameManager
+        currentApp = ((App)GameManager.instance.apps.Dequeue()); // Sets the next App as currentApp
+        setApp(currentApp); // Updates the screen with the new currentApp
+    }
+
+    public void Reject()
+    {
+        Debug.Log("Rejected!");
+        GameManager.instance.AppChoice(currentApp, false);
+        currentApp = ((App)GameManager.instance.apps.Dequeue());
+        setApp(currentApp);
+
+    }
+
+        // Update is called once per frame
+
+
+    
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -54,9 +77,18 @@ public class UIManager : MonoBehaviour
         }
     }
 
+    
+
+    
+
     public void setApp(App app){
         titleField.text = app.getTitle();
         descriptionField.text = app.getDescripton();
         imageField.GetComponent<Image>().sprite = app.getImage();
+
+
     }
+
+
+
 }
