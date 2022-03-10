f5e73863be860751ec4fa77aa33a0a6c68c9fb14
delinor <54844513+delinor@users.noreply.github.com>
2022-03-10 11:28:47 +0100

    Full functionality for the json-file apps-class in progress
    Edited the name of json-files and copied these so that they are in the correct path. Made changes in UI-manager that are out-commented until the game fully works with these new methods.

--- TheAppStoreGame/Assets/Scripts/UIManager.cs
+++ TheAppStoreGame/Assets/Scripts/UIManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using JsonHandler;
+using Newtonsoft;
+using System.IO;
 
 public class UIManager : MonoBehaviour
 {
@@ -16,25 +19,48 @@ public class UIManager : MonoBehaviour
     public App app2;
     private App currentApp;
     private PlayerController controller;
+    private Apps[] testApps;
+    private Apps testApp;
+    public Queue appsQueue; 
+    private Apps newCurrentApp;
    
     // Start is called before the first frame update
+
+    /* OBS! Current methods currently using the old class for applications,
+    new methods will be implemented or are already implemented but out-commented
+    in order for the game to work with full functionality. */
+
     void Start()
     {
         controller = new PlayerController();
+        
+        //Loads the json-file and creates an array with applications.
+        StreamReader r = new StreamReader("jsonString.json");
+        string jsonString = r.ReadToEnd();
+        testApps = Apps.FromJson(jsonString);
+
+        appsQueue = new Queue(testApps);
 
         titleField = GameObject.Find("Title").GetComponent<Text>();
         descriptionField = GameObject.Find("Description").GetComponent<Text>();
 
 
         //GameManager.instance.Awake(); 
-
         currentApp = ((App)GameManager.instance.apps.Dequeue());
+        newCurrentApp = (Apps)appsQueue.Dequeue(); 
+        //newCurrentApp will replace currentApp when all methods work with the new Apps-class
+
         setApp(currentApp);
+        //setApp2(newCurrentApp); //will replace setApp
     }
 
     public void Accept()
     {
         Debug.Log("Accepted!");
+
+        /*newCurrentApp = (Apps)appsQueue.Dequeue(); 
+        setApp2(newCurrentApp);*/ 
+
         GameManager.instance.AppChoice(currentApp, true); // Saves currentApp to GameManager
         if (GameManager.instance.apps.Count == 0)
         {
@@ -45,15 +71,16 @@ public class UIManager : MonoBehaviour
             currentApp = ((App)GameManager.instance.apps.Dequeue()); // Sets the next App as currentApp
             GameManager.instance.newspaperDisplay();
             setApp(currentApp); // Updates the screen with the new currentApp
-        }
-       
-
-        
+        } 
     }
 
     public void Reject()
     {
         Debug.Log("Rejected!");
+        
+        /* newCurrentApp = (Apps)appsQueue.Dequeue();
+        setApp2(newCurrentApp); */
+
         GameManager.instance.AppChoice(currentApp, false);
         if (GameManager.instance.apps.Count == 0)
         {
@@ -97,16 +124,17 @@ public class UIManager : MonoBehaviour
     }
     */
 
-    
-
-    
-
     public void setApp(App app){
         titleField.text = app.getTitle();
         descriptionField.text = app.getDescripton();
         imageField.GetComponent<Image>().sprite = app.getImage();
+    }
 
-
+    //will replace setApp
+    public void setApp2(Apps app){
+        titleField.text = app.Title;
+        descriptionField.text = app.Description;
+        imageField.GetComponent<Image>().sprite = Resources.Load<Sprite>(app.Images[0]);
     }
 
 
