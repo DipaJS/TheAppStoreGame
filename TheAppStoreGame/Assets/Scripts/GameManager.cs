1e90a1cfe8827ef01d3d68be32e9c898e8a68877
DipaJS <70640380+DipaJS@users.noreply.github.com>
2022-03-31 17:41:06 +0200

    Email WiP
    Added red notification when you get email

--- TheAppStoreGame/Assets/Scripts/GameManager.cs
+++ TheAppStoreGame/Assets/Scripts/GameManager.cs
@@ -19,7 +19,7 @@ public class GameManager : MonoBehaviour
     {
         get
         {
-            if(_instance == null)
+            if (_instance == null)
             {
                 Debug.Log("GameManager is NULL");
             }
@@ -43,6 +43,8 @@ public class GameManager : MonoBehaviour
     public GameObject newsPopup;
     public GameObject newspaperModel;
 
+    // Components and objects for the email
+    public GameObject emailNotification;
     public GameObject textMail;
     public GameObject nameMail;
 
@@ -66,40 +68,40 @@ public class GameManager : MonoBehaviour
     //  bool accepted   - true if the App is to be accepted, false if it is rejected
     public void AppChoice(Apps app, bool accepted)
     {
-        app.Status = accepted;  
+        app.Status = accepted;
         evaluatedApps.Enqueue(app);
-        newspaperModel.SetActive(true); 
+        newspaperModel.SetActive(true);
     }
 
     /* Uses the most recently evaluated application and checks if there are any consequences that should be 
     displayed, if there are - this method calls for the corresponding Display-method (e.g. NewspaperDisplay) */
     // Method called in UIManager.Evaluate
-    public void LoadConsequences(){
+    public void LoadConsequences() {
 
         Apps app;
-        Consequence[] consequences; 
+        Consequence[] consequences;
         Consequence newsPaperConsequence = null;
         Consequence emailConsequence = null;
 
-        if (evaluatedApps.Count != 0){                                  
+        if (evaluatedApps.Count != 0) {
             app = (Apps)evaluatedApps.Dequeue(); //app is the most recently reviewed application
 
             //Checks if the application was accepted or rejected and loads the corresponding consqequences to variable         
-            if (app.Status){consequences = app.Accepted.Consequences;}  
-            else {consequences = app.Rejected.Consequences;}
+            if (app.Status) { consequences = app.Accepted.Consequences; }
+            else { consequences = app.Rejected.Consequences; }
 
             //Checks where each consequence should be displayed and saves them in the corresponding variable
-                //Implemented as a switch method so it is easy to add more display locations in future development
-            foreach (Consequence c in consequences){
-                switch (c.DisplayLocation){
-                case 1: newsPaperConsequence = c; break;
-                case 2: emailConsequence = c; break;
+            //Implemented as a switch method so it is easy to add more display locations in future development
+            foreach (Consequence c in consequences) {
+                switch (c.DisplayLocation) {
+                    case 1: newsPaperConsequence = c; break;
+                    case 2: emailConsequence = c; break;
                 }
                 Debug.Log("Test: " + newsPaperConsequence.TextToDisplay);
             }
             //Calls for the corresponding Display-method if there are any new consequences to display in that location
-            if (newsPaperConsequence != null){NewspaperDisplay(newsPaperConsequence, app);}
-            if (emailConsequence != null){EmailDisplay(emailConsequence, app);}
+            if (newsPaperConsequence != null) { NewspaperDisplay(newsPaperConsequence, app); }
+            if (emailConsequence != null) { EmailDisplay(emailConsequence, app); }
         }
     }
     // Loads the newspaper with consquence text and title+image for the corresponding application
@@ -111,10 +113,12 @@ public class GameManager : MonoBehaviour
         imageField.sprite = Resources.Load<Sprite>(app.Images[0]);
     }
 
-    public void EmailDisplay(Consequence c, Apps app){
+    public void EmailDisplay(Consequence c, Apps app) {
         //Load the parameters of consequence c to a new Email in the Email-tab
-        nameMail.GetComponent<TextMeshProUGUI>().text = c.Name;
-        textMail.GetComponent<TextMeshProUGUI>().text = c.TextToDisplay;
+
+            emailNotification.GetComponent<CanvasGroup>().alpha = 1;
+            nameMail.GetComponent<TextMeshProUGUI>().text = c.Name;
+            textMail.GetComponent<TextMeshProUGUI>().text = c.TextToDisplay;
 
     }
 }
